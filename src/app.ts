f96b0d04b3b7efb73fbe78392cc7a9fdca4f0a1f
Albert Pivnenko <pivnenko.albert@gmail.com>
2023-03-16 06:46:57 +0200

    no changes

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,19 @@
 import { FavoritesView, MainView } from "src/views/index";
 import { AppState } from "./interfaces/index";
 
-type Pages = MainView | FavoritesView
+type Pages = MainView | FavoritesView;
 
 class App {
   appState: AppState = {
     favorites: [],
   }
+  
   private currentView: Pages;
   private routes = [
     { path: "", view: MainView },
     { path: "#favorites", view: FavoritesView },
   ];
+
   constructor() {
     window.addEventListener("hashchange", this.route.bind(this));
     this.route();
